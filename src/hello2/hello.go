24a9597ecb8fbf58d48271ff791f1eb449371daa
Sergei Udris <sergei.udris@gmail.com>
2021-02-04 11:50:28 +0300

    return a map of name:gretting

--- src/hello2/hello.go
+++ src/hello2/hello.go
@@ -12,10 +12,15 @@ func main() {
     // the log entry prefix and a flag to disable printing
     // the time, source file, and line number.
     log.SetPrefix("greetings: ")
-    log.SetFlags(0)
+	log.SetFlags(0)
+	
+	// A slice of names.
+	names := []string{"Gladys", "Samantha", "Darrin"}
+
 
     // Request a greeting message.
-    message, err := greetings.Hello("Gladys")
+	// message, err := greetings.Hello("Gladys")
+	messages, err := greetings.Hellos(names)
     // If an error was returned, print it to the console and
     // exit the program.
     if err != nil {
@@ -24,5 +29,5 @@ func main() {
 
     // If no error was returned, print the returned message
     // to the console.
-    fmt.Println(message)
+    fmt.Println(messages)
 }
