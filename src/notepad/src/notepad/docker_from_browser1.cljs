401ea667b7f60942ecbb8e02a06007ba7b3573ea
Sergei Udris <sergei.udris@gmail.com>
2021-02-16 15:58:57 +0300

    .vscode: add ipfs-cube to wrokspace

---
+++ src/notepad/src/notepad/docker_from_browser1.cljs
@@ -0,0 +1,21 @@
+(ns notepad.docker-from-browser1
+  (:require
+   [clojure.core.async :as a :refer [chan go go-loop <! >!  take! put! offer! poll! alt! alts! close!
+                                     pub sub unsub mult tap untap mix admix unmix pipe
+                                     timeout to-chan  sliding-buffer dropping-buffer
+                                     pipeline pipeline-async]]
+   [cljs.core.async.impl.protocols :refer [closed?]]
+   [cljs.core.async.interop :refer-macros [<p!]]
+   [goog.string.format :as format]
+   [goog.string :refer [format]]
+   [goog.object]
+   [clojure.string :as str]
+   [cljs.reader :refer [read-string]]))
+
+
+(comment
+
+
+
+  ;;
+  )
